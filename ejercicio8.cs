d0de84d516d77acfa5754018c908b5de60d3a822
Angel Noh <angelgabrielnohcamargo@gmail.com>
2025-03-24 17:23:05 -0600

    Add files via upload

---
+++ ejercicio8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numeros = new List<int>();
+
+        Console.WriteLine("Ingresa 10 números:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            int num = int.Parse(Console.ReadLine());
+            numeros.Add(num);
+        }
+
+        Console.Write("\nIngresa el número que deseas eliminar: ");
+        int numeroEliminar = int.Parse(Console.ReadLine());
+
+     
+        if (numeros.Contains(numeroEliminar))
+        {
+            numeros.Remove(numeroEliminar);
+            Console.WriteLine($"\nEl número {numeroEliminar} fue eliminado.");
+        }
+        else
+        {
+            Console.WriteLine($"\nEl número {numeroEliminar} no se encontró en la lista.");
+        }
+
+        Console.WriteLine("\nLista actualizada:");
+        foreach (int num in numeros)
+        {
+            Console.Write(num + " ");
+        }
+
+        Console.WriteLine(); 
+    }
+}
